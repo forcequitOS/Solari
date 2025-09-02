366b9fcdd5bcd28d887856bad7960859cbe953c0
Taj • forcequitOS <62081155+forcequitOS@users.noreply.github.com>
2025-09-01 21:39:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Solari
+Enable Liquid Glass from Swift Playgrounds. It's really the simplest thing imaginable.
+
+Call `enableLiquidGlass()` in your app and it'll be Liquid Glass now. Or use the `.enableLiquidGlass()` SwiftUI view modifier if you're lazy like me. 
+
+This is built for Swift Playgrounds but should hypothetically work literally anywhere. It obviously still required i(Pad)OS 26.
+
+Package requires appleOS '19 or later, since believe it or not, it uses SwiftUI. Should work on iOS, macOS, visionOS, watchOS, and tvOS, but I cannot truly be bothered testing it on all of those.
---
+++ Sources/Solari/Solari.swift
@@ -0,0 +1,21 @@
+// This is literally the world's simplest possible package. I truly do not even need to make a package for this.
+// All this does is set one UserDefaults key to force Liquid Glass support.
+// Built to be utilized from within Swift Playground, which at the moment, has zero Liquid Glass support.
+
+import SwiftUI
+import Foundation
+
+public func enableLiquidGlass() {
+    let key = "com.apple.SwiftUI.IgnoreSolariumLinkedOnCheck"
+    if UserDefaults.standard.object(forKey: key) == nil {
+        UserDefaults.standard.set(true, forKey: key)
+    }
+}
+
+public extension View {
+    func enableLiquidGlass() -> some View {
+        self.onAppear {
+            enableLiquidGlass()
+        }
+    }
+}
